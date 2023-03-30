3129e811e5a2d9a0db50a52259f8684a4105f56d
shiva3638 <119850364+shiva3638@users.noreply.github.com>
2023-03-30 11:48:21 +0530

    Update vars.tf
    code commit

--- vars.tf
+++ vars.tf
@@ -1,3 +1,17 @@
 variable REGION {
     default = "ap-south-1"
 }
+
+variable AMI {
+    type = map
+    default = {
+        ap-south-1 = "ami-0376ec8eacdf70aae"
+        us-east-1 = "**"
+    }
+}
+
+
+variable type {
+    default = "t2.micro"
+  
+}
